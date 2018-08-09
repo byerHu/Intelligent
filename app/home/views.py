659746f96ab2d1192531c5f92a6a65973528e2a2
byerHu <1369392606@qq.com>
2018-08-09 17:42:40 +0800

    change index.html

--- app/home/views.py
+++ app/home/views.py
@@ -2,10 +2,15 @@
 from . import home  # 从当前目录导入home蓝图
 from flask import render_template
 
-
+# 首页
 @home.route("/")
 def index():
     return render_template('home/index.html')
+
+# 搜索页
+@home.route('/search/')
+def search():
+    return render_template('home/search.html')
 
 
 @home.route('/legalprovision/')
@@ -23,9 +28,9 @@ def login():
     return render_template('home/login.html')
 
 
-@home.route('/list/')
-def list():
-    return render_template('home/list.html')
+@home.route('/case/judge')
+def case_judge():
+    return render_template('home/case_judge.html')
 
 
 @home.route('/judgmentdoc/')
