2158b98b285bc8c899d9d81f12f1c81c90f44781
byerHu <1369392606@qq.com>
2018-08-11 16:47:40 +0800

    change detail.html

--- app/home/views.py
+++ app/home/views.py
@@ -12,27 +12,32 @@ def index():
 def search():
     return render_template('home/search.html')
 
+# 条文详情页
+@home.route('/provision/detail')
+def provision_detail():
+    return render_template('home/provision_detail.html')
 
+# 法律条文
 @home.route('/legalprovision/')
 def legalprovision():
     return render_template('home/legalprovision.html')
 
-
+# 注册页
 @home.route('/register/')
 def register():
     return render_template('home/register.html')
 
-
+# 登录页
 @home.route('/login/')
 def login():
     return render_template('home/login.html')
 
-
+# 案件研判
 @home.route('/case/judge')
 def case_judge():
     return render_template('home/case_judge.html')
 
-
+# 判决文书
 @home.route('/judgmentdoc/')
 def judgmentdoc():
     return render_template('home/judgmentdoc.html')
